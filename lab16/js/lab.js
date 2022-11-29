2bb4ca8213ac7193cbd8d6bcda640a0b94365d93
aeliasro <aeliasro@ucsc.edu>
2022-11-28 22:00:28 -0800

    lab 16 stuff

---
+++ lab16/js/lab.js
@@ -0,0 +1,35 @@
+/**
+*Author: Kyra and Arianna
+*Created: November 23, 2022
+*License: Public Domain
+**/
+
+// Using the core $.ajax() method
+$.ajax({
+    // The URL for the request (from the api docs)
+    url: "https://xkcd.com/info.0.json",
+    // The data to send (will be converted to a query string)
+    data: {
+            // here is where any data required by the api
+            //   goes (check the api docs)
+            id: 2704,
+          },
+    // Whether this is a POST or GET request
+    type: "GET",
+    // The type of data we expect back
+    dataType : "json",
+    // What do we do when the api call is successful
+    //   all the action goes in here
+    success: function(data) {
+        // do stuff
+        console.log(data);
+        var title = data.name
+        var imgage = "imgs.xkcd.com/comics/faucet.png";
+
+    },
+    // What we do if the api call fails
+    error: function (jqXHR, textStatus, errorThrown) {
+        // do stuff
+        console.log("Error:", textStatus, errorThrown);
+    }
+})
